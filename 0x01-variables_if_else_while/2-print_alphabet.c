5e38827e2ab1da6bfe38f927e3f1962082c4a3d0
ismael <spacingismael@gmail.com>
2023-09-08 20:02:56 +0100

    m

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,7 +4,6 @@
  * main - Entry point
  *
  * Return: Always 0 (Success)
- * 
  */
 
 int main(void)
@@ -12,11 +11,11 @@ int main(void)
 	char end = 'z';
 	char start = 'a';
 
-	while (start <= end) {
+	while (start <= end)
+	{
 		putchar(start);
 		start++;
 	}
-	
 	putchar('\n');
 	return (0);
 }
