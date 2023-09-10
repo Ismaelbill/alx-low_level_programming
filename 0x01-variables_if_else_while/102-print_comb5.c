799aed01a732ad7ce950fadfdf6bf8c5b2297ba2
ismael <spacingismael@gmail.com>
2023-09-10 17:35:33 +0100

    m

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,25 +8,26 @@
 
 #include <stdio.h>
 
-int main(void) {
-    int i, j;
+int main(void)
+{
+	int i, j;
 
-    for (i = 0; i <= 99; i++)
-    {
-        for (j = i + 1; j <= 99; j++)
+	for (i = 0; i <= 99; i++)
 	{
-		putchar((i / 10) + '0');
-		putchar((i % 10) + '0');
-		putchar(' ');
-		putchar((j / 10) + '0');
-		putchar((j % 10) + '0');
-		if (i != 98 || j != 99)
+		for (j = i + 1; j <= 99; j++)
 		{
-			putchar(',');
+			putchar((i / 10) + '0');
+			putchar((i % 10) + '0');
 			putchar(' ');
-		}
-        }
-    }
-    putchar('\n');
-    return (0);
+			putchar((j / 10) + '0');
+			putchar((j % 10) + '0');
+			if (i != 98 || j != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+        	}
+	}
+	putchar('\n');
+	return (0);
 }
