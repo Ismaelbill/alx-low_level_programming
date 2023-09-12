093776d17cbbd96f9278b966479d8546e04a1e35
ismael <spacingismael@gmail.com>
2023-09-13 00:19:14 +0100

    m

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,27 +1,30 @@
 #include "main.h"
 
 /**
- * _isalpha - function to check if
- *	      input is alphabet
+ * print_sign - print + if n is greater than zero.
+ *		0 if n is zero and - if n is less
+ *		than zero.
  *
- * @c: checks input of function
+ * @n: takes integer type input for function
  *
- * Return: returns 1 if 'c' is alphabet
- *	   otherwise always 0 (Success)
+ * Return: returns 1 if +, 0 if 0 and -1 if -
  */
 
 int print_sign(int n)
 {
-    if (n < 0)
-    {
-        _putchar('-');
-        return (-1);
-    } else if (n == 0)
-    {
-        _putchar('0');
-        return (0);
-    }else {
-        _putchar('+');
-        return (1);
-    }
+	if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('+');
+		return (1);
+	}
 }
