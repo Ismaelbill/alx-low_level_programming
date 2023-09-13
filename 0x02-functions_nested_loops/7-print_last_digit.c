3015de8383d3498bc4238d9b87f9c6d67ade5693
ismael <spacingismael@gmail.com>
2023-09-13 01:04:01 +0100

    m

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_last_digit - function print the last digit of variable n.
+ *
+ * @n: takes number input
+ *
+ * Return: lastDigit
+ */
+
+int print_last_digit(int n)
+{
+	int lastDigit;
+
+	if (n < 0)
+	{
+		n = n * -1;
+	}
+	lastDigit = n % 10;
+	_putchar(lastDigit + '0');
+	return (lastDigit);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 
+
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -10,4 +11,5 @@ int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
+int print_last_digit(int);
 #endif
