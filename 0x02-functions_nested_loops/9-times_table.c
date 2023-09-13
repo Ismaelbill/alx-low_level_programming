0202cb4a8173ec9739387db9782bf4eef3d286c6
ismael <spacingismael@gmail.com>
2023-09-13 02:56:09 +0100

    m

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,13 +13,13 @@ void times_table(void)
 		_putchar(48);
 		_putchar(',');
 		_putchar(' ');
-		_putchar(' ');
 		for (j = 1; j <= 9; j++)
 		{
 			int result = i * j;
 
 			if (result < 10)
 			{
+				_putchar(' ');
 				_putchar(result + '0');
 			} else
 			{
@@ -29,7 +29,6 @@ void times_table(void)
 			if (j < 9)
 			{
 				_putchar(',');
-				_putchar(' ');
 				_putchar(' ');
 			} else
 			{
