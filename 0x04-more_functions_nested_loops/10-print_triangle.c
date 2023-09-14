345c1447924fa6fa79be157bc8bcd747ce48275d
ismael <spacingismael@gmail.com>
2023-09-14 16:46:28 +0100

    m

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -13,18 +13,18 @@ void print_triangle(int size)
 
 	if (size == 0)
 	{
-		_putchar('\n');
+		printf("\n");
 	}
 	for (i = 1; i <= size; i++)
 	{
 		for (j = size - i; j > 0; j--)
 		{
-			_putchar(' ');
+			printf(" ");
 		}
 		for (k = 1; k <= i; k++)
 		{
-			_putchar('#');
+			printf("#");
 		}
-		_putchar('\n');
+		printf("\n");
 	}
 }
