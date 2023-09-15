f01b575b0609f0fb6b8fea1e2042cc4b0f0fa77a
ismael <spacingismael@gmail.com>
2023-09-15 10:51:54 +0100

    m

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -12,12 +12,16 @@ int main(void)
 
 	unsigned long int i, current = 1, previous = 0, tmp = 0;
 
-	printf("%ld", previous);
-	printf(", %ld", current);
 	for (i = 0; i < 49; i++)
 	{
 		tmp = current + previous;
-		printf(", %ld", tmp);
+		if (i == 48)
+		{
+			printf("%ld", tmp);
+			break;
+		}
+		
+		printf("%ld, ", tmp);
 		previous = current;
 		current = tmp;
 	}
