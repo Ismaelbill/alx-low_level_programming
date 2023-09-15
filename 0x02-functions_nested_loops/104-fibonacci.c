a2ce3030653c4d61387e905fc8d98571e17c7570
ismael <spacingismael@gmail.com>
2023-09-15 11:21:17 +0100

    m

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * main - function that prints the first 98 fibonacci numbers
+ *
+ * Return: 0 (Success)
+*/
+
+
+int main(void)
+{
+
+	unsigned long int i, current = 1, previous = 0, tmp = 0;
+
+	for (i = 0; i < 89; i++)
+	{
+		tmp = current + previous;
+		if (i == 49)
+		{
+			printf("%ld", tmp);
+			break;
+		}
+		printf("%ld, ", tmp);
+		previous = current;
+		current = tmp;
+	}
+	printf("\n");
+	return (0);
+}
