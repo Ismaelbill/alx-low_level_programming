f5d83c9aa04793e324a7e843f21f9c4b6c942920
ismael <spacingismael@gmail.com>
2023-09-18 16:20:29 +0100

    m

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - funtion that prints a string, followed by a new line, to stdout
+ *
+ * @str: parameter of _puts
+*/
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		write(STDOUT_FILENO, str, 1);
+		str++;
+	}
+	write(STDOUT_FILENO, "\n", 1);
+}
