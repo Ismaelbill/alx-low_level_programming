19e84be4d21cfc08cc51c25269001f23511d13db
ismael <spacingismael@gmail.com>
2023-09-18 16:59:02 +0100

    m

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,17 +1,24 @@
 #include "main.h"
 
+/**
+ * print_rev - function that reverse the ouput
+ *
+ * @s: parameter of print_rev
+ */
+
 void print_rev(char *s)
 {
-    int length = 0, i;
-    while (*s != '\0')
+	int length = 0, i;
+
+	while (*s != '\0')
 	{
-        s++;
-        length++;
+		s++;
+		length++;
 	}
-    for (i = length; i > 0; i--)
-    {
-        s--;
-        write(STDOUT_FILENO, s, 1);
-    }
-    write(STDOUT_FILENO, "\n", 1);
+	for (i = length; i > 0; i--)
+	{
+		s--;
+		write(STDOUT_FILENO, s, 1);
+	}
+	write(STDOUT_FILENO, "\n", 1);
 }
