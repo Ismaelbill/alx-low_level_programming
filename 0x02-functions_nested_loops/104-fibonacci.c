349462d6b43194a79f2e596f7fdaa25b159652dc
ismael <spacingismael@gmail.com>
2023-09-18 14:53:02 +0100

    m

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,7 +9,6 @@
 
 int main(void)
 {
-/*
 	unsigned long  i, current = 1, previous = 0, tmp = 0;
 
 	for (i = 0; i <= 89; i++)
@@ -24,23 +23,6 @@ int main(void)
 		previous = current;
 		current = tmp;
 	}
-	printf("\n");*/
-	unsigned long int a = 1, b = 2;
-    int count = 0;
-
-    printf("%lu, %lu", a, b);
-    count += 2;
-
-    while (count < 98) {
-        unsigned long next = a + b;
-
-        printf(", %lu", next);
-        count++;
-
-        a = b;
-        b = next;
-    }
-
-    printf("\n");
+	printf("\n");
     return (0);
 }
