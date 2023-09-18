262a03e8d609ac5d836ebe7b2e1d303eceb60d4d
ismael <spacingismael@gmail.com>
2023-09-18 20:12:00 +0100

    m

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,15 +9,14 @@
 void rev_string(char *s)
 {
 	int i = 0, l;
-    char temp;
+	char temp;
+
 	for (l = 0; s[l] != '\0'; ++l)
-    ;
-    
-	
-    for (i = 0; i < l / 2; i++)
-    {
-        temp = s[i];
-        s[i] = s[l - 1 - i];
-        s[l - 1 - i] = temp;
-    }
+		;
+	for (i = 0; i < l / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[l - 1 - i];
+		s[l - 1 - i] = temp;
+	}
 }
