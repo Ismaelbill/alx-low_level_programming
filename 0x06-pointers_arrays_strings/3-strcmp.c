47f830153caa86852d6d959321ea93d170de1ba3
ismael <spacingismael@gmail.com>
2023-09-21 06:38:02 +0100

    m

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,17 +14,27 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int result = 0;
 
-	while (*s1 != '\0')
+	while (*s1 != '\0' && *s2 != '\0')
 	{
-		if (*s1 != *s2)
+		if (*s1 < *s2)
 		{
-			result = ((int)*s1 - 48) - ((int)*s2 - 48);
-			break;
+			return -1;
+		} else if (*s1 > *s2)
+		{
+			return 1;
 		}
 		s1++;
 		s2++;
 	}
-	return (result);
+
+	if (*s1 == '\0' && *s2 != '\0')
+	{
+			return -1;
+	}else if (*s1 != '\0' && *s2 == '\0')
+	{
+			return 1;
+	}
+
+	return (0);
 }
