e19c56c423e60b3f1327837c891e258483b49af6
ismael <spacingismael@gmail.com>
2023-09-21 06:22:56 +0100

    m

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,16 +14,15 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int result;
+	int result = 0;
 
-	while (*s1 != '\0' && *s2 != '\0')
+	while (*s1 != '\0')
 	{
-		if (s1 < s2)
-			result = -15;
-		else if (s1 > s2)
-			result = 15;
-		else
-			result = 0;
+		if (*s1 != *s2)
+		{
+			result = ((int)*s1 - 48) - ((int)*s2 - 48);
+			break;
+		}
 		s1++;
 		s2++;
 	}
