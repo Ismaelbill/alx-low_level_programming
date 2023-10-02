4034594ae7e866e12b64b28b73039bea868e1a0f
ismael <spacingismael@gmail.com>
2023-10-02 23:44:51 +0100

    m

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -16,8 +16,9 @@ int main(int argc, char *argv[])
 	while (--argc)
 	{
 		/*for (ch = argv[argc]; *ch; ch++)*/
-		{
 			ch = argv[argc];
+		if (*ch)
+        {
 			if (*ch < '0' || *ch > '9')
 				return (printf("Error\n"), 1);
 			ch++;
