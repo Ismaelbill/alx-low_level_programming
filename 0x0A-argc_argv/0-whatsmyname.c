695c18e9db96eb52d1bdc2cd5b169417d80e1d44
ismael <spacingismael@gmail.com>
2023-10-02 18:18:50 +0100

    m

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 
-/***/
+/**
+ * main - program that prints its name
+ * @argc: the size of argv array
+ * @argv: an array of size argc
+ * Return: returns (0) in Success
+ */
 
-int main(int __attribute__ ((unused)) argc, char* argv[])
+int main(int __attribute__ ((unused)) argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
