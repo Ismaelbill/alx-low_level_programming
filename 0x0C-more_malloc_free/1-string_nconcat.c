4a1c90107c02db4eba2a8e9161b3fb58b7d7c42a
ismael <spacingismael@gmail.com>
2023-10-07 15:52:08 +0100

    m

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,49 +13,30 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *str;
-	unsigned int i, j, result, calc;
+	unsigned int i, j;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 	for (i = 0; s1[i] != '\0'; i++)
-		i++;
-	for (j = 0; j < n; j++)
-		j++;
+		;
+	for (j = 0; s2[j]; j++)
+		;
 
-	if (j <= n)
-	{
-		str = malloc((i + j) * sizeof(char)+1);
+	
+		str = malloc((i + n) * sizeof(char) + 1);
 		if (str == NULL)
 			return (NULL);
 
 		for (i = 0; s1[i] != '\0'; i++)
 			str[i] = s1[i];
-		for (j = 0; s2[j] != '\0'; j++)
+		for (j = 0; j < n; j++)
         {
 			str[i] = s2[j];
             i++;
         }
 		str[i] = '\0';
-	}
-	else
-	{
-		result = j - n;
-		calc = j - result;
-
-		str = malloc((i + calc) * sizeof(char)+1);
-		if (str == NULL)
-			return (NULL);
-
-		for (i = 0; s1[i] != '\0'; i++)
-			str[i] = s1[i];
-		for (j = 0; j < calc; j++)
-        {
-			str[i] = s2[j];
-            i++;
-        }
-		str[i] = '\0';
-	}
+	
 	return (str);
 }
