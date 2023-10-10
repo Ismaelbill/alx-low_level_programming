bcf8156f0e3edfb1cada0bf543add07fcbe71563
ismael <spacingismael@gmail.com>
2023-10-10 18:07:44 +0100

    m

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - function prints name of the current file
+ * Return: returns (0) in Success
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
