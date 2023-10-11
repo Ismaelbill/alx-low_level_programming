dee4f7c0e7430a1bd5c6ad5c5109ef815624b313
ismael <spacingismael@gmail.com>
2023-10-11 01:23:05 +0100

    m

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,16 +10,30 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    dog_t *my_dog;
-    if (!name || age < 0 || !owner)
-        return (NULL);
-    
-    my_dog = (dog_t *)malloc(sizeof(dog_t));
+	dog_t *my_dog;
 
-    if(my_dog == NULL)
-        return (NULL);
-    my_dog->name = strdup(name);
-    my_dog->age = age;
-    my_dog->owner = strdup(owner);
-    return my_dog;
+	if (!name || age < 0 || !owner)
+		return (NULL);
+
+	my_dog = (dog_t *)malloc(sizeof(dog_t));
+
+	if (my_dog == NULL)
+		return (NULL);
+
+	my_dog->name = malloc(sizeof(char) * (strlen(name) + 1));
+	if (my_dog->name == NULL)
+	{
+		free(my_dog);
+		return (NULL);
+	}
+	my_dog->owner = malloc(sizeof(char) * (strlen(owner) + 1));
+	if (my_dog->owner == NULL)
+	{
+		free(my_dog);
+		return (NULL);
+	}
+	my_dog->name = strcpy(my_dog->name, name);
+	my_dog->age = age;
+	my_dog->owner = strcpy(my_dog->owner, owner);
+	return (my_dog);
 }
