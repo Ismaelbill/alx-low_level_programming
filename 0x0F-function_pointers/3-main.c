1b18b45a7aaa58b832d0ee81e11cc07136d8e11e
ismael <spacingismael@gmail.com>
2023-10-12 15:15:53 +0100

    m

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -12,16 +12,15 @@ int main(int argc, char **argv)
 	int (*ptrFunc)(int, int), a, b;
 
 	if (argc != 4)
-		printf("Error!\n"), exit(98);
+		printf("Error\n"), exit(98);
 
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
 
 	ptrFunc = get_op_func(argv[2]);
 	if (!ptrFunc)
-		printf("Error!\n"), exit(99);
-	if ((b == 0) && ((argv[2][0] == '/') || (argv[2][0] == '%')))
-		printf("Error!\n"), exit(100);
+		printf("Error\n"), exit(99);
+
 	printf("%d\n", ptrFunc(a, b));
 	return (0);
 }
