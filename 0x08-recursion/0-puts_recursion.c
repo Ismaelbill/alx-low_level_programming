7c953299ab0d5807dec97602f2dcaa98b3e8afb0
ismael <spacingismael@gmail.com>
2023-10-25 12:25:26 +0100

    m

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,17 +2,17 @@
 
 /**
  * _puts_recursion - function that prints a string
- * followed by a new line
- * @s: string to print
+ * @s: pointer to string
  */
+
 
 void _puts_recursion(char *s)
 {
-	if (*s != '\0')
+	if (!*s)
 	{
-		write(STDOUT_FILENO, s, 1);
-		_puts_recursion(s + 1);
+		_putchar('\n');
+		return;
 	}
-	else
-		write(STDOUT_FILENO, "\n", 1);
+	_putchar(*s);
+	_puts_recursion(++s);
 }
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,10 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-#include <unistd.h>
-#include <math.h>
-int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
@@ -13,5 +9,6 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
+int _putchar(char c);
 
 #endif
