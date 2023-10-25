42d399f31623a79656be909449b660c1ca09ddcc
ismael <spacingismael@gmail.com>
2023-10-25 18:04:17 +0100

    m

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,26 +1,33 @@
 #include "main.h"
-#include <stdio.h>
-/***/
 
-int equals(int n, int i, int j, int result)
+/**
+ * func - funtion returns the natural square root of a number
+ * @n: number
+ * @i: number
+ * Return: square root, if 'n' doesn't have square root
+ * returns -1
+ */
+
+int func(int n, int i)
 {
-	if (n)
-	{
-		result = i * j;
-		return equals(n, i++, j++);
-	}
-	if (result == n)
-	{
-		printf("hello");
-		return 1;
-	}
-
+	if ((i * i) == n)
+		return (i);
+	else if ((i * i) > n)
+		return (-1);
+	else
+		return (func(n, i + 1));
 }
+
+/**
+ * _sqrt_recursion - function that returns the natural square root of a number
+ * @n: number
+ * Return: the number that returned from 'func' function
+ */
+
 
 int _sqrt_recursion(int n)
 {
-	int i = 1, j = 1, result;
-	equals(n, i, j, result);
-	return 1;
+	int i = 1;
 
+	return (func(n, i));
 }
