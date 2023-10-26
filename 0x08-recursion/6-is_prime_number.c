93a44a91bac40278b032de732e16b7e98b8663db
ismael <spacingismael@gmail.com>
2023-10-26 01:18:48 +0100

    m

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,12 +1,34 @@
 #include "main.h"
 
-/***/
+/**
+ * func - funtion use recursion for prime numbers
+ * @n: number
+ * @i: number
+ * Return: return (1) if it's prime, otherwise
+ * (0).
+ */
+
+int func(int n, int i)
+{
+	if (n <= 1)
+		return (0);
+	else if (n % i == 0 && i < n)
+		return (0);
+	else if (i == n)
+		return (1);
+	else
+		return (func(n, i + 1));
+}
+
+/**
+ * is_prime_number - function that returns 1 if
+ * the input integer is a prime number, otherwise return 0.
+ * @n: number
+ * Return: returns the returned value from 'func' function
+ */
 
 int is_prime_number(int n)
 {
-
-
-
-
+	return (func(n, 2));
 }
 
