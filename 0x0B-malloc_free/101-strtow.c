b0c03c346258959d5f07ea2e8eb2b2ea9640a8ae
ismael <spacingismael@gmail.com>
2023-11-01 23:47:36 +0100

    still on this bug

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -8,7 +8,7 @@
 
 char **strtow(char *str)
 {
-	int i, k, j, l, countWord = -1, wordStart = 0;
+	int i, k, j, l, countWord = 0, wordStart = 0;
 	char **ptr;
 
 	if (str == NULL || str[0] == '\0')
@@ -20,17 +20,17 @@ char **strtow(char *str)
 				(str[i] != ' ' && str[i + 1] == '\0'))
 			countWord++;
 	}
-	ptr = malloc(sizeof(char *) * countWord + 1);
+	ptr = malloc(sizeof(char *) * countWord);
 	if (ptr == NULL)
 		return (NULL);
-	for (i = 0; i < countWord + 1; i++)
+	for (i = 0; i < (countWord); i++)
 	{
 		while (str[wordStart] == ' ')
 			wordStart++;
 		j = 0;
 		while (str[wordStart + j] != ' ' && str[wordStart + j] != '\0')
 			j++;
-		ptr[i] = malloc(sizeof(char) * j + 1);
+		ptr[i] = malloc(j + 1);
 		if (ptr[i] == NULL)
 		{
 			for (k = 0; k < i; k++)
@@ -44,6 +44,6 @@ char **strtow(char *str)
 		ptr[i][l] = '\0';
 		wordStart += j;
 	}
-	ptr[countWord + 1] = NULL;
+	ptr[countWord] = NULL;
 	return (ptr);
 }
