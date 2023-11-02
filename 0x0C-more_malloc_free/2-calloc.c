7fa23c6d748bc822a4b857399991a5c7eba78cd2
ismael <spacingismael@gmail.com>
2023-11-02 21:07:33 +0100

    testing

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,7 +10,7 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	unsigned int i;
-	char *ptr;
+	int *ptr;
 	
 	if (nmemb == 0 || size == 0)
 		return(NULL);
@@ -20,7 +20,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (ptr == NULL)
 		return (NULL);
 	for (i = 0; i < nmemb; i++)
-		ptr[i] = 0;
+		ptr[i] = '0';
 	/*_memset(ptr, 0, sizeof(int*/
 
 	return (ptr);
