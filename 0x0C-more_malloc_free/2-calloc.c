ffde1e78088648f0b6c51b338db09d2ca0720998
ismael <spacingismael@gmail.com>
2023-11-02 20:36:15 +0100

    fixing task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,17 +11,14 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	unsigned int i;
 	char *ptr;
-
+	(void)i;
 	if (nmemb == 0 || size == 0)
-		return (NULL);
+		return(NULL);
 
 	ptr = malloc(nmemb * size);
 
 	if (ptr == NULL)
 		return (NULL);
-
-	for (i = 0; i < nmemb; i++)
-		ptr[i] = 0;
 
 	return (ptr);
 }
