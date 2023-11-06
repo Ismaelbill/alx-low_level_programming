827da2085b0e5780ba18a8086adc02d2ad34ef10
ismael <spacingismael@gmail.com>
2023-11-06 14:11:07 +0100

    task 4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,59 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * length - function returns the length of the string
+ * @s: string
+ * Return: returns length
+ */
+int length(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+/**
+ * new_dog - function that creates a new dog.
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ * Return: returns the object, otherwise NULL
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	int i;
+
+	if (!(name) || !(owner))
+		return (NULL);
+	dog_t *ne_dog = malloc(sizeof(dog_t));
+
+	if (ne_dog == NULL)
+		return (NULL);
+
+	ne_dog->name = malloc(length(name) + 1);
+
+	if (ne_dog->name == NULL)
+	{
+		free(ne_dog);
+		return (NULL);
+	}
+	for (i = 0; i <= length(name); i++)
+		ne_dog->name[i] = name[i];
+
+	ne_dog->age = age;
+
+	ne_dog->owner = malloc(length(owner) + 1);
+
+	if (ne_dog->owner == NULL)
+	{
+		free(ne_dog);
+		return (NULL);
+	}
+	for (i = 0; i <= length(owner); i++)
+		ne_dog->owner[i] = owner[i];
+
+	return (ne_dog);
+}
