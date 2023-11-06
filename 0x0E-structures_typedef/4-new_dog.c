f4ab83d93388caa9b7f7b723bd0935258eb0b11b
ismael <spacingismael@gmail.com>
2023-11-06 15:47:06 +0100

    another custom

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -25,17 +25,22 @@ int length(char *s)
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	int i;
+	dog_t *ne_dog;
 
-	dog_t *ne_dog = malloc(sizeof(dog_t));
+	if (name == NULL || age < 0 || owner == NULL)
+		exit(1);
 
-	if (ne_dog == NULL || !(name) || !(owner) || age < 0)
+	ne_dog = malloc(sizeof(*ne_dog));
+
+	if (ne_dog == NULL)
+	{
 		return (NULL);
+	}
 
 	ne_dog->name = malloc(sizeof(char) * length(name) + 1);
 
 	if (ne_dog->name == NULL)
 	{
-		free(ne_dog->name);
 		free(ne_dog);
 		return (NULL);
 	}
@@ -49,12 +54,13 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	if (ne_dog->owner == NULL)
 	{
-		free(ne_dog->owner);
+		free(ne_dog->name);
 		free(ne_dog);
 		return (NULL);
 	}
 	for (i = 0; owner[i]; i++)
 		ne_dog->owner[i] = owner[i];
 	ne_dog->owner[i] = '\0';
+
 	return (ne_dog);
 }
