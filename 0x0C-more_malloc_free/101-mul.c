40c9ebd3d384e1529f8d969654e22943d394e8ba
ismael <spacingismael@gmail.com>
2023-11-07 15:08:47 +0100

    another custom

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -9,12 +9,14 @@
 
 int main(int argc, char *argv[])
 {
-	if (argc > 3 || argc <= 1)
+	int result;
+
+	if (argc > 3 || argc <= 2)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
-
+	result = atoi(argv[1]) * atoi(argv[2]);
+	printf("%d\n", result);
 	return (0);
 }
