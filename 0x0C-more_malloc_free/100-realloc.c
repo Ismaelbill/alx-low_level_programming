bb4a2e556a72dc1c018a0da1aa5947741e1aaf06
ismael <spacingismael@gmail.com>
2023-11-07 06:11:19 +0100

    task 100

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -10,5 +10,24 @@
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-
+	if (old_size == new_size)
+		return (ptr);
+	if (ptr == NULL)
+	{
+		ptr = malloc(new_size);
+		if (ptr == NULL)
+			return (NULL);
+	}
+	if (new_size > old_size)
+	{
+		ptr = malloc(new_size);
+		if (ptr == NULL)
+			return (NULL);
+	}
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		exit(1);
+	}
+	return (ptr);
 }
