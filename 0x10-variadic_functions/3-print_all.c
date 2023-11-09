05bf32223f57f394ee6b1a5368badef10d1a0cbf
ismael <spacingismael@gmail.com>
2023-11-09 10:40:47 +0100

    custom in task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -12,57 +12,36 @@ void func(int i, int length, const char * const format);
 
 void print_all(const char * const format, ...)
 {
-	unsigned int i = 0, length = 0;
-	char *s;
+	unsigned int i = 0;
+	char *s, *sep = "";
 	va_list args;
 
 	va_start(args, format);
 
-	while (format[i])
-	{
-		if ((format[i] == 'c' || format[i] == 'i' ||
-			format[i] == 'f' || format[i] == 's'))
-			length++;
-		i++;
-	}
-
-	i = 0;
-
-	while (format[i])
+	while (format && format[i])
 	{
 		switch (format[i])
 		{
 			case 'c':
-				printf("%c", va_arg(args, int));
+				printf("%s%c", sep, va_arg(args, int));
 				break;
 			case 'i':
-				printf("%i", va_arg(args, int));
+				printf("%s%i", sep, va_arg(args, int));
 				break;
 			case 'f':
-				printf("%f", va_arg(args, double));
+				printf("%s%f", sep, va_arg(args, double));
 				break;
 			case 's':
 				s = va_arg(args, char *);
-				printf("%s", s ? s : "(nil)");
+				printf("%s%s", sep, s ? s : "(nil)");
 				break;
+			default:
+				i++;
+				continue;
 		}
-		func(i, length, format);
+		sep = ", ";
 		i++;
 	}
 	printf("\n");
 	va_end(args);
-}
-
-/**
- * func - function print comma and space after the arguments
- * accept the last one
- * @i: number
- * @l: length of 'f'
- * @f: list of arguments
- */
-void func(int i, int l, const char * const f)
-{
-	if (i < l && (f[i] == 'c' || f[i] == 'i'
-		|| f[i] == 'f' || f[i] == 's'))
-		printf(", ");
 }
