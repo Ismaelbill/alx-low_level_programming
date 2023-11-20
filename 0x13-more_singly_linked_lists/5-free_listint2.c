92c8a995368ca0512eeb2ec3b2b0b47034d14ac6
ismael <spacingismael@gmail.com>
2023-11-20 22:59:08 +0100

    task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - function that frees a listint_t list
+ * @head: ptr to ptr to struct
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp, *current;
+
+	tmp = *head;
+	while (tmp)
+	{
+		current = tmp->next;
+		free(tmp);
+		tmp = current;
+	}
+	*head = NULL;
+}
