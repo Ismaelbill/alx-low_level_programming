d42b33ce8ffb5e45a3be35a68e48c7b3b450a391
ismael <spacingismael@gmail.com>
2023-11-20 17:56:13 +0100

    fixing directory name

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_listint - function that prints all the elements
+ * of a listint_t list.
+ * @h: pointer to struct
+ * Return: returns the number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	int i = 0;
+
+	while (h)
+	{
+		i++;
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (i);
+}
