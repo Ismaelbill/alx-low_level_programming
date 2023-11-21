3dff12da80ea3ddbd79629589e9dc5806f121022
ismael <spacingismael@gmail.com>
2023-11-21 02:41:56 +0100

    task 6

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * pop_listint - function that deletes the head node of a listint_t
+ * @head: ptr to ptr to struct
+ * Return: the head node's data (n)
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *newNode;
+	int i = 0;
+
+	if (*head == NULL)
+		return (0);
+	newNode = *head;
+
+	(*head) = newNode->next;
+
+	while (newNode)
+	{
+		i++;
+		newNode = newNode->next;
+	}
+	return (i);
+}
