1e5aed6eb35a66c1279f4e89ec77ef300b2fa8ad
ismael <spacingismael@gmail.com>
2023-11-22 01:03:17 +0100

    task 10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -26,7 +26,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	{
 		current = current->next;
 		index--;
-		if (!prev)
+		if (!current)
 			return (-1);
 	}
 	prev = current->next;
