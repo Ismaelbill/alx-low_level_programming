eb0e70b4d706774cf6c4d26d077b2a463651d92f
ismael <spacingismael@gmail.com>
2023-11-24 10:14:39 +0100

    task 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,6 +1,18 @@
 #include "main.h"
 
 /**
+ * print_rec - function print recursively
+ * @n: number
+ */
+void print_rec(unsigned long n)
+{
+	if (n > 1)
+		print_rec(n >> 1);
+	_putchar((n & 1) + '0');
+}
+
+
+/**
  * print_binary - function that prints the binary representation
  * of a number
  * @n: number
@@ -8,26 +20,8 @@
 
 void print_binary(unsigned long int n)
 {
-	int num = 0, i = 0;
-	char b[100] = "";
-
 	if (n == 0)
 		_putchar('0');
-	while (n > 0)
-	{
-		num = n & 1;
-		if (num == 1)
-			b[i] = '1';
-		else
-			b[i] = '0';
-		n >>= 1;
-		i++;
-	}
-	num = 0;
-	while (b[num])
-	{
-		_putchar(b[i - 1]);
-		i--;
-		num++;
-	}
+	else
+		print_rec(n);
 }
