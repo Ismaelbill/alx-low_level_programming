6141c6af7add57732d85d2f824216a4f595dc8d7
ismael <spacingismael@gmail.com>
2023-11-24 22:22:48 +0100

    task 4

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * set_bit - function that sets the value of a bit
+ * to 0 at a given index
+ * @n: number
+ * @index: number
+ * Return: 1 if it worked, -1 if an error occurred
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	int res = 0;
+
+	if (index < sizeof(unsigned long int) * 8)
+	{
+		res = ~(1UL << index);
+		*n &= res;
+		return (1);
+	}
+	return (-1);
+}
