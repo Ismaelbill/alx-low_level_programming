958e7007ad45ae75bf35a67cde6e86fa2b38c420
ismael <spacingismael@gmail.com>
2023-11-24 22:23:35 +0100

    task 4 finished

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * set_bit - function that sets the value of a bit
+ * clear_bit - function that sets the value of a bit
  * to 0 at a given index
  * @n: number
  * @index: number
