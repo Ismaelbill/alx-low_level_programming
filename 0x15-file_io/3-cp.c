b8186922c68581d522a370664ea2e61bbb82816d
ismael <spacingismael@gmail.com>
2023-11-28 23:14:02 +0100

    task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,8 +1,9 @@
 #include "main.h"
 #define BUFF_SIZE 1024
 #define ERR_FILE "Usage: cp file_from file_to\n"
-#define ERR_FILE_FROM "Error: Can't read from file %s\n"
-#define ERR_FILE_TO "Error: Can't read from file %s\n"
+#define ERR_FILE_FROM_TO "Error: Can't read from file %s\n"
+#define NO_CLOSE "Error: Can't close fd %i\n"
+#define NO_WRITE "Error: Can't write to %s\n"
 /**
  * main - function that copies the content of a file to another file
  * @argc: size of argv
@@ -21,27 +22,24 @@ int main(int argc, char *argv[])
 
 	fileNum = open(argv[1], O_RDONLY);
 	if (fileNum == -1)
-		dprintf(STDERR_FILENO, ERR_FILE_FROM, argv[1]), exit(98);
-	fileNum2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+		dprintf(STDERR_FILENO, ERR_FILE_FROM_TO, argv[1]), exit(98);
+	fileNum2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 
 	if (fileNum2 == -1)
-	{
-		dprintf(STDERR_FILENO, ERR_FILE_TO, argv[2]);
-		exit(99);
-	}
+		dprintf(STDERR_FILENO, NO_WRITE, argv[2]), exit(99);
 
 	while ((b = read(fileNum, buffer, BUFF_SIZE)) > 0)
 		if (write(fileNum2, buffer, b) != b)
-			dprintf(STDERR_FILENO, ERR_FILE_TO, argv[2]), exit(99);
+			dprintf(STDERR_FILENO, NO_WRITE, argv[2]), exit(99);
 	if (b == -1)
-		dprintf(STDERR_FILENO, ERR_FILE_FROM, argv[1]), exit(98);
+		dprintf(STDERR_FILENO, ERR_FILE_FROM_TO, argv[1]), exit(98);
 
 	fileNum = close(fileNum);
 	fileNum2 = close(fileNum2);
 
 	if (fileNum)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", fileNum), exit(100);
+		dprintf(STDERR_FILENO, NO_CLOSE, fileNum), exit(100);
 	if (fileNum2)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", fileNum2), exit(100);
+		dprintf(STDERR_FILENO, NO_CLOSE, fileNum2), exit(100);
 	return (0);
 }
