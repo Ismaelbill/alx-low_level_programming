025b875850391644eca3256126c8d80172015738
ismael <spacingismael@gmail.com>
2023-11-28 22:49:04 +0100

    task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,11 +1,13 @@
 #include "main.h"
 #define BUFF_SIZE 1024
-
+#define ERR_FILE "Usage: cp file_from file_to\n"
+#define ERR_FILE_FROM "Error: Can't read from file %s\n"
+#define ERR_FILE_TO "Error: Can't read from file %s\n"
 /**
  * main - function that copies the content of a file to another file
  * @argc: size of argv
  * @argv: array of size argc
- * Return: can return 97 or 98 or 99 or 100
+ * Return: return 0 in success
  */
 
 int main(int argc, char *argv[])
@@ -15,30 +17,31 @@ int main(int argc, char *argv[])
 	ssize_t b;
 
 	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+		dprintf(STDERR_FILENO, ERR_FILE), exit(97);
+
 	fileNum = open(argv[1], O_RDONLY);
 	if (fileNum == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-	b = read(fileNum, buffer, (BUFF_SIZE));
+		dprintf(STDERR_FILENO, ERR_FILE_FROM, argv[1]), exit(98);
 	fileNum2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
 	if (fileNum2 == -1)
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
-	write(fileNum2, buffer, strlen(buffer));
+	{
+		dprintf(STDERR_FILENO, ERR_FILE_TO, argv[2]);
+		exit(99);
+	}
+
+	while ((b = read(fileNum, buffer, BUFF_SIZE)) > 0)
+		if (write(fileNum2, buffer, b) != b)
+			dprintf(STDERR_FILENO, ERR_FILE_TO, argv[2]), exit(99);
 	if (b == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
+		dprintf(STDERR_FILENO, ERR_FILE_FROM, argv[1]), exit(98);
 
 	fileNum = close(fileNum);
 	fileNum2 = close(fileNum2);
 
 	if (fileNum)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %s\n", argv[1]), exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", fileNum), exit(100);
 	if (fileNum2)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %s\n", argv[2]), exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", fileNum2), exit(100);
 	return (0);
 }
